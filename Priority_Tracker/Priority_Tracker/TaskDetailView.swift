0bf51e42400cf8c37e6141995e3232e2b7eeb0c4
Steffi Lin <steffilin@berkeley.edu>
2022-11-18 18:11:36 -0800

    added scrollview and tap to dismiss keyboard for better user experience

--- Priority_Tracker/Priority_Tracker/TaskDetailView.swift
+++ Priority_Tracker/Priority_Tracker/TaskDetailView.swift
@@ -23,81 +23,87 @@ struct TaskDetailView: View {
 
     
     var body: some View {
-        
-        VStack {
-            Text("Task Details")
-                .font(Font.mainTitle)
-                .multilineTextAlignment(.leading)
-                .frame(width: 318.0, height: 50.0, alignment: .leading)
-                .padding(.bottom)
-            ZStack {
-                Rectangle()
-                    .frame(width: 318, height: 50)
-                    .cornerRadius(10)
-                    .foregroundColor(Color.bg_gray)
-                TextField("Title", text: $task.title)
-                    .frame(width: 318.0 - 28, height: 50.0 - 26)
-                    .focused($textIsFocused)
-            }
-            .padding(.bottom)
-            
-            ZStack {
-                Rectangle()
-                    .frame(width: 318, height: 50)
-                    .cornerRadius(10)
-                    .foregroundColor(Color.bg_gray)
-                DatePicker("Due", selection: $task.due_date)
-                    .frame(width: 318.0 - 28, height: 50.0 - 26)
-                    .focused($textIsFocused)
-            }
-            .padding(.bottom)
-            
-            ZStack {
-                Rectangle()
-                    .frame(width: 318, height: 251)
-                    .cornerRadius(10)
-                    .foregroundColor(Color.bg_gray)
-                VStack {
-                    TextEditorWithPlaceholder(text: $task.description)
+        ScrollView {
+            VStack {
+                Text("Task Details")
+                    .font(Font.mainTitle)
+                    .multilineTextAlignment(.leading)
+                    .frame(width: 318.0, height: 50.0, alignment: .leading)
+                    .padding(.bottom)
+                ZStack {
+                    Rectangle()
+                        .frame(width: 318, height: 50)
+                        .cornerRadius(10)
+                        .foregroundColor(Color.bg_gray)
+                    TextField("Title", text: $task.title)
+                        .frame(width: 318.0 - 28, height: 50.0 - 26)
                         .focused($textIsFocused)
-                        .textSelection(.enabled)
                 }
-            }
-            .padding(.bottom)
-            
-            
-            Button("Save") {
-                showHome.toggle()
-//                    presentation.wrappedValue.dismiss()
-//                    ContentView.showingHome = false
+                .padding(.bottom)
                 
-            }.padding()
-                .frame(width: 137.0, height: 50.0)
-                .foregroundColor(.white)
-                .background(Color.accentColor)
-                .cornerRadius(10)
-                .font(Font.buttonText)
-                .frame(width: 318.0, height: 50.0, alignment: .leading)
-                .disabled(task.title.isEmpty)
-                .fullScreenCover(isPresented: $showHome) {
-//                        withoutAnimation {
-//                            ContentView(tasks: Task.tasks)
-//                        }
-                    ContentView(tasks: $tasks) {
-                        TaskStore.save(tasks: store.tasks) { result in
-                            if case .failure(let error) = result {
-                                fatalError(error.localizedDescription)
+                ZStack {
+                    Rectangle()
+                        .frame(width: 318, height: 50)
+                        .cornerRadius(10)
+                        .foregroundColor(Color.bg_gray)
+                    DatePicker("Due", selection: $task.due_date)
+                        .frame(width: 318.0 - 28, height: 50.0 - 26)
+                        .focused($textIsFocused)
+                }
+                .padding(.bottom)
+                
+                ZStack {
+                    Rectangle()
+                        .frame(width: 318, height: 251)
+                        .cornerRadius(10)
+                        .foregroundColor(Color.bg_gray)
+                    VStack {
+                        TextEditorWithPlaceholder(text: $task.description)
+                            .focused($textIsFocused)
+                            .textSelection(.enabled)
+                    }
+                }
+                .padding(.bottom)
+                
+                
+                Button("Save") {
+                    showHome.toggle()
+    //                    presentation.wrappedValue.dismiss()
+    //                    ContentView.showingHome = false
+                    
+                }.padding()
+                    .frame(width: 137.0, height: 50.0)
+                    .foregroundColor(.white)
+                    .background(Color.accentColor)
+                    .cornerRadius(10)
+                    .font(Font.buttonText)
+                    .frame(width: 318.0, height: 50.0, alignment: .leading)
+                    .disabled(task.title.isEmpty)
+                    .fullScreenCover(isPresented: $showHome) {
+    //                        withoutAnimation {
+    //                            ContentView(tasks: Task.tasks)
+    //                        }
+                        ContentView(tasks: $tasks) {
+                            TaskStore.save(tasks: store.tasks) { result in
+                                if case .failure(let error) = result {
+                                    fatalError(error.localizedDescription)
+                                }
                             }
                         }
+                        
                     }
                     
-                }
-                
-            Spacer()
-           
+                Spacer()
+               
+            }
+            .onChange(of: scenePhase) { phase in
+                if phase == .inactive { saveAction() }
+            }
         }
-        .onChange(of: scenePhase) { phase in
-            if phase == .inactive { saveAction() }
+        
+        .onTapGesture {
+            hideKeyboard()
+            textIsFocused = false
         }
     }
 }
